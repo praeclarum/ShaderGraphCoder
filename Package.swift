bd46857cb9bcee979f95e92cbd9c80e792f072de
Frank A. Krueger <fak@praeclarum.org>
2024-02-19 15:51:09 -0800

    More platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ShaderGraphCoder",
-    platforms: [.visionOS(.v1)],
+    platforms: [.visionOS(.v1), .iOS(.v13), .watchOS(.v6), .tvOS(.v13), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
