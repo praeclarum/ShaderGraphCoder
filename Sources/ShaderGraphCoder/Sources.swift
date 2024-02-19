51deaaccbd9f3b8faac0566594aaec93313624e9
Frank A. Krueger <fak@praeclarum.org>
2024-02-19 15:47:47 -0800

    Add more parameter types

--- Sources/ShaderGraphCoder/Sources.swift
+++ Sources/ShaderGraphCoder/Sources.swift
@@ -18,6 +18,12 @@ public extension SGValue {
     static func color3fParameter(name: String, defaultValue: SIMD3<Float>, colorSpace: SGColorSpace = .textureSRGB) -> SGColor {
         SGColor(source: .parameter(name: name, defaultValue: .color3f(defaultValue, colorSpace: colorSpace)))
     }
+    static func color4f(_ x: Float, _ y: Float, _ z: Float, _ w: Float, colorSpace: SGColorSpace = .textureSRGB) -> SGColor {
+        SGColor(source: .constant(.color4f([x, y, z, w], colorSpace: colorSpace)))
+    }
+    static func color4fParameter(name: String, defaultValue: SIMD4<Float>, colorSpace: SGColorSpace = .textureSRGB) -> SGColor {
+        SGColor(source: .parameter(name: name, defaultValue: .color4f(defaultValue, colorSpace: colorSpace)))
+    }
     static func float(_ value: Float) -> SGScalar {
         SGScalar(source: .constant(.float(value)))
     }
@@ -57,17 +63,26 @@ public extension SGValue {
     static func vector2f(_ x: Float, _ y: Float) -> SGVector {
         SGVector(source: .constant(.vector2f([x, y])))
     }
+    static func vector2fParameter(name: String, defaultValue: SIMD2<Float>) -> SGVector {
+        SGVector(source: .parameter(name: name, defaultValue: .vector2f(defaultValue)))
+    }
     static func vector3f(_ value: SIMD3<Float>) -> SGVector {
         SGVector(source: .constant(.vector3f(value)))
     }
     static func vector3f(_ x: Float, _ y: Float, _ z: Float) -> SGVector {
         SGVector(source: .constant(.vector3f([x, y, z])))
     }
+    static func vector3fParameter(name: String, defaultValue: SIMD3<Float>) -> SGVector {
+        SGVector(source: .parameter(name: name, defaultValue: .vector3f(defaultValue)))
+    }
     static func vector4f(_ value: SIMD4<Float>) -> SGVector {
         SGVector(source: .constant(.vector4f(value)))
     }
     static func vector4f(_ x: Float, _ y: Float, _ z: Float, _ w: Float) -> SGVector {
         SGVector(source: .constant(.vector4f([x, y, z, w])))
+    }
+    static func vector4fParameter(name: String, defaultValue: SIMD4<Float>) -> SGVector {
+        SGVector(source: .parameter(name: name, defaultValue: .vector4f(defaultValue)))
     }
     static var worldCameraPosition: SGVector {
         SGVector(source: SGValueSource.nodeOutput(SGNode.worldCameraPosition, "out"))
